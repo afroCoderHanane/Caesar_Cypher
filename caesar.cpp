0abfc14afc033a4de17a05bdf0a2ecb2b9b003e5
abdoul gbadamassi <gbadhanane@gmail.com>
2019-11-23 00:33:30 +0000

    commit

--- caesar.cpp
+++ caesar.cpp
@@ -2,29 +2,31 @@
 
 caesar_cipher::caesar_cipher()
 {
-
+ msg = "";
+ key =0;
 }
 caesar_cipher::caesar_cipher(string encrypt_this)
 {
-
+ msg = encrypt_this;
+ key = 0;
 }
 void caesar_cipher::setKey(int key_value)
 {
-
+ key =keyvalue;
+}
+int caesar_cipher::getKey()
+{
+ return key;
 }
 void caesar_cipher::set_encryptedtext(string message)
 {
-
+ 
 }
 string caesar_cipher::get_encryptedtext()
 {
-
+ rerun
 }
 
-int caesar_cipher::getKey()
-{
-
-}
 string encryptedtext()
 {
 
