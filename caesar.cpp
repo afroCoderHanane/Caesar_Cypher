208eeb611ebf4db0a9c23fb7006c28cfcd421a63
abdoul gbadamassi <gbadhanane@gmail.com>
2019-11-25 04:25:47 +0000

    mm-- commit

--- caesar.cpp
+++ caesar.cpp
@@ -1,64 +1,41 @@
+#include "caesar.h"
 //created by Abdoul-hanane on 11/23/19
 #include"caesar.h"
 
 caesar_cipher::caesar_cipher()
 {
 	msg = "";
-	key = 0;
+	
+	key = 4;
 }
 caesar_cipher::caesar_cipher(string encrypt_this)
 {
 	msg = encrypt_this;
-	//key = 0;
 }
 void caesar_cipher::setKey(int key_value)
 {
-  //•	set_key(int key_value). A mutator method that sets the new key value
 	key = key_value;
 }
 int caesar_cipher::getKey()
 {
-  //•	get_key(). An accessor method that returns the current key value
 	return key;
 }
-//read to understand more//
-void caesar_cipher::set_encryptedtext(string msg)
-{ // •	set_encryptedtext(string message. A mutator method that sets the encrypted text
-
-	// int n = message.length();
-	// for (int i = 0; i < n; i++)
-	// {
-	// 	if (isalpha(message[i]))
-	// 	{
-	// 		int offset = 65;
-	// 		if (islower(message[i]))
-	// 			offset = 97;
-	// 		int cipheredchar = ((((int)message[i]) - offset + key) % 26) + offset;
-	// 		cout << (char)cipheredchar;
-	// 	}
-	// 	else
-	// 		cout << message[i];
-	// }
-
-  msg =get_encryptedtext();
+void caesar_cipher::set_encryptedtext(string message)
+{
+	msg = message;
 }
 
 string caesar_cipher::get_encryptedtext()
 {
-	
 	return msg;
 }
 
 string caesar_cipher::encrypt()
 {
-	//msg = encrypt(encrypt_this);
-		return msg;
+	return msg;
 }
 void caesar_cipher::encrypt(string encrypt_this)
-{
-	//set_encryptedtext(encrypt_this);
-  //function that encrypt
-  int n = encrypt_this.length();
+{    int n = encrypt_this.length();
 	for (int i = 0; i < n; i++)
 	{
 		if (isalpha(encrypt_this[i]))
@@ -67,19 +44,15 @@ void caesar_cipher::encrypt(string encrypt_this)
 			if (islower(encrypt_this[i]))
 				offset = 97;
 			int cipheredchar = ((((int)encrypt_this[i]) - offset + key) % 26) + offset;
-			cout << (char)cipheredchar;
-      msg[i]= (char)cipheredchar;
+			msg[i]= (char)cipheredchar;
 		}
 		else
-			cout << encrypt_this[i];
+			msg[i] = encrypt_this[i];
 	}
 }
 
 void caesar_cipher::decrypt()
 {
-	//string enmsg = encrypt();
-  cout<< endl;
-  cout<< get_encryptedtext()<< endl;
 	for (int i = 0; i < msg.length(); i++)
 	{
 		if (isalpha(msg[i]))
@@ -87,19 +60,15 @@ void caesar_cipher::decrypt()
 			int offset = 65;
 			if (islower(msg[i]))
 				offset = 97;
-			int decipheredchar = ((((int)msg[i]) + offset - key) % 26 ) + offset;
-      msg[i] =(char)decipheredchar;
-      //cout<< " I passed by";
-			cout <<(char)decipheredchar;
-     
+			int cipheredchar = ((((int)msg[i]) - offset - key) % 26) + offset;
+			cout << (char)cipheredchar;
 		}
 		else
 			cout << msg[i];
-      
 	}
 }
+/*
 void caesar_cipher::query_for_message(string prompt)
 {
 	cout << prompt << endl;
-  //return prompt;
-}
+}*/
