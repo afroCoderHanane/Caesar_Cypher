d7171b47f1284b56ec705ed5f83fbb2a7249da2c
Abdoul Hanane <gbadhanane@gmail.com>
2019-12-17 03:43:45 +0000

    try&catch

--- caesar.cpp
+++ caesar.cpp
@@ -1,66 +1,78 @@
 #include "caesar.h"
-//created by hananeDev84 on 11/23/19
-
-caesar_cipher::caesar_cipher()
-{msg = "";key = 4;}
-caesar_cipher::caesar_cipher(string encrypt_this)
-{msg = encrypt_this;}
+//created by @hananeDev84 on 11/23/19
+caesar_cipher::caesar_cipher():msg(""),key(4){}//default constructor to initalize the message and the key 
+caesar_cipher::caesar_cipher(string encrypt_this):msg(encrypt_this){}// constructor to set the message to encrypt
 void caesar_cipher::setKey(int key_value)
-{key = key_value;}
-int caesar_cipher::getKey()
-{	return key;}
-void caesar_cipher::set_encryptedtext(string message)
-{msg = message;}
-
-string caesar_cipher::get_encryptedtext()
-{return msg;}
-
-string caesar_cipher::encrypt()
-{return msg;}
-void caesar_cipher::encrypt(string encrypt_this)
-{    int n = encrypt_this.length();
-	for (int i = 0; i < n; i++){
-		if (isalpha(encrypt_this[i]))
-		{
-			int offset = 65;
-			if (islower(encrypt_this[i]))
-				offset = 97;
-		  int cipheredchar = ((((int)encrypt_this[i]) - offset +26 + key) % 26) + offset;
-			msg[i]= (char)cipheredchar;
-		}
-		else if (isdigit(encrypt_this[i]))
-		{int cipheredchar = ((((int)encrypt_this[i]) - 38 + key) % 10) + 48;
-			msg[i] = (char)cipheredchar;
-		}
-		else
-			msg[i] = encrypt_this[i];
-	}
-}
-
-void caesar_cipher::decrypt()
 {
+  key=key_value;// method that set they key
+}
+int caesar_cipher::getKey()
+{
+  return key; //method to get the key
+}
+void caesar_cipher::set_encryptedtext(string message)
+{
+  msg=message;//method that set the message to encrypt to msg
+}
+string caesar_cipher::get_encryptedtext()
+{
+  return msg;// method to get the tecxt to encrypt
+}
+string caesar_cipher::encrypt()
+{
+  return msg;
+}
+void caesar_cipher::encrypt(string encrypt_this)
+{ 
+  int offset;
+  int n = encrypt_this.length();
+  int cipheredchar;
+	for (int i = 0; i < n; i++)
+  {
+     if (isalpha(encrypt_this[i]))
+	    	{ 
+            offset= 65;
+			      if (islower(encrypt_this[i]))
+			   	     offset = 97;
+		 cipheredchar = ((((int)encrypt_this[i]) - offset +26 + key) % 26) + offset;
+		 msg[i]= (char)cipheredchar;
+	  	}
+		else if (isdigit(encrypt_this[i]))
+	  	{ 
+        cipheredchar = ((((int)encrypt_this[i]) - 38 + key) % 10) + 48;
+		  	msg[i] = (char)cipheredchar;
+	  	}
+		else
+		  	msg[i] = encrypt_this[i];
+	    }
+}
+void caesar_cipher::decrypt()
+{	
+  int offset;
+  int decipheredchar;
 	for (int i = 0; i < msg.length(); i++)
-	{
-		if (isalpha(msg[i]))
-		{
-			int offset = 65;
-			if (islower(msg[i]))
-				offset = 97;
-			int cipheredchar = ((((int)msg[i]) - offset+26 - key) % 26) + offset;
-			cout << (char)cipheredchar;
+    	{
+         if (isalpha(msg[i]))
+		        { 
+              offset = 65;
+			   if (islower(msg[i]))
+				      offset = 97;
+	decipheredchar = ((((int)msg[i]) - offset+26 - key) % 26) + offset;
+	cout << (char)decipheredchar;
 		}
 		else if (isdigit(msg[i]))
 		{
-			int cipheredchar = ((((int)msg[i]) - 38 - key) % 10) + 48;
-			cout << (char)cipheredchar;
+        decipheredchar = ((((int)msg[i]) - 38 - key) % 10) + 48;
+		   	cout << (char)decipheredchar;
 		}
 		else
-			cout << msg[i];
+		  	cout << msg[i];
 	}
 }
 string caesar_cipher::query_for_message(string prompt)
-{
-	cout << "Enter your prompt" << endl;
-	getline(cin,prompt);
-	return prompt;
+{ 
+    cout << "Enter your prompt" << endl;
+    getline(cin,prompt);
+    if(prompt.empty())throw string("Empty String");
+    return prompt;
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
-// ciphercode.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ciphercode.cpp : This file contains the 'main' function. Program execution
+// begins and ends there.
 //
 
+#include "caesar.h"
 #include <iostream>
-#include"caesar.h"
 using namespace std;
 
-int main()
-{   
-	string h1; 
-	caesar_cipher test;
-	h1 = test.query_for_message(h1);
-	test.set_encryptedtext(h1);
-	cout << "Text entered" << endl<< test.get_encryptedtext() << endl;
-	test.setKey(1);
-	cout << "key = " << test.getKey() << endl;
-	test.encrypt(h1);cout << endl;
-	cout << "Encrypted text: " <<endl<< test.encrypt() << endl;
-	cout << "The decrypted text is : "<<endl;test.decrypt();
-}
 
+
+int main() {
+string h1 ;
+caesar_cipher test;
+int key;
+  try{
+          h1 = test.query_for_message(h1);
+          test.set_encryptedtext(h1);
+          cout << "Text entered" << endl << test.get_encryptedtext() << endl << endl;
+          cout << "Enter a key" << endl;
+     }
+  catch(string str1)
+      {
+          cout<<str1<<endl;
+          exit(2);
+      }
+
+	try {
+          if (key < -11 || key > 11)
+            throw string("Enter a valid key");
+          else
+            cin >> key;
+	    } 
+  catch (string str)
+      {
+		       cout << str << endl;
+	        	exit(1);
+	    }
+test.setKey(key);
+cout << "key = " << test.getKey() << endl;
+test.encrypt(h1);
+
+cout <<endl<< "Encrypted text: " << test.encrypt() << endl;
+cout << "The decrypted text is : " <<" ";
+test.decrypt();
+
+	return 0;
+}
